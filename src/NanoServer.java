74f829cd78181da1828d59c5775f065f6283029f
Jon Eikholm <jone@kea.dk>
2019-12-17 15:54:32 +0100

    s

--- src/NanoServer.java
+++ src/NanoServer.java
@@ -2,13 +2,13 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
-
+// Github: https://bit.ly/36Ic6o5
 public class NanoServer {
     public static void main(String[] args) {
         try {
             ServerSocket serverSocket = new ServerSocket(5555);
             // open connection from clients:
-            while (true) {
+            while (true) { // move this line !!
                 Socket socket = serverSocket.accept(); // accept() blocks until there is a conn.
                 System.out.println("connected!");
                 // create a "normal" scanner, to read network data
